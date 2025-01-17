4c9a50f2beac4ad091a0181820d826b9b196759d
nk <khakholia.nk@gmail.com>
2025-01-17 14:57:14 +0530

    collecting all requirements and test cases

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("git setup")
+// Requirements
+// 1.  Empty string returns 0
+// 2.  Returns the sum of the numbers in a string
+// 3.  Supports any amount of numbers
+// 4.  Supports only positive numbers
+// 5.  Takes care of '\n' in given ',' seperated string 
+// 6.  Supports any delimiter given at the start of string in the format '//[delimiter]\n....'
+// 7.  Numbers greater than 1000 are ignored
+// 8.  Delimiters can be of any length with the following format: “//[delimiter]\n”
+// 9.  Allow multiple delimiters like this: “//[delim1][delim2]\n”
+// 10.  Make sure you can also handle multiple delimiters with length longer than one char
