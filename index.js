8dd11b1d423a7af81b5c3e7e682017d428d10d8d
nk <khakholia.nk@gmail.com>
2025-01-17 17:39:10 +0530

    code formatting for readibality

--- index.js
+++ index.js
@@ -15,7 +15,9 @@
 // Returns sum of string and supports above cases
 const sumOfString = (str) => {
     try {
+        // empty string check
         if (!str) return 0
+
         //delimiters support 
         if (str.startsWith("//")) {
             str = str.replace("//", "")
@@ -27,6 +29,7 @@ const sumOfString = (str) => {
             while ((match = regex.exec(str)) !== null) {
                 delimiters.push(match[1]);
             }
+
             // replacing delimiters with ','
             delimiters.forEach(delimiter => {
                 str = str.replace(`[${delimiter}]`, "")
@@ -34,13 +37,18 @@ const sumOfString = (str) => {
             })
             str = str.slice(1)
         }
+
         // supporting \n and ,
         str = str.replace("\n", ",")
+
         // more than 1000 numbers are ignored
         str = str.split(",").map(s => Number(s)).filter(s => s <= 1000)
+
         // negetives check
         var negetives = str.filter(s => s < 0)
         if (negetives.length) throw new Error("negative numbers not allowed " + negetives.join(","))
+
+        // returning sum
         return str.reduce((acc, curr) => acc + curr, 0)
     } catch (e) {
         console.error("sumOfString", e)
@@ -52,19 +60,68 @@ import { assert } from "chai"
 // Test cases for 'sumOfString' function defined above
 const testSumOfString = () => {
     try {
-        assert.equal(sumOfString(""), 0, "Test Case 1  *** Empty string should return 0 ***")
-        assert.equal(sumOfString("2,3"), 5, "Test Case 2  *** Sum of 2,3 should return 5 ***")
-        assert.equal(sumOfString("2,3,4,5,6,7,8,9,10,99"), (2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 99), "Test Case 3 *** Sum of 2,3,4,5,6,7,8,9,10,99 should return 153 ***")
-        assert.throws(() => sumOfString("-1,1,-3"), Error, "negative numbers not allowed -1,-3", "Test Case 4 *** Negative numbers are not allowed ***")
-        assert.equal(sumOfString("2,3\n4"), 9, "Test Case 5 *** Sum of 2,3,4 should return 9 ***")
-        assert.equal(sumOfString("//[*],2,3\n4"), 9, "Test Case 6 *** Sum of 2,3,4 should return 9 ***")
-        assert.equal(sumOfString("1000,2000,3,4"), 1007, "Test Case 7 *** Sum of 1000,2000,3,4 should return 1007 ***")
-        assert.equal(sumOfString("//[**],2**3\n4"), 9, "Test Case 8 *** Sum of 2,3,4 should return 9 ***")
-        assert.equal(sumOfString("//[#],2#3\n4"), 9, "Test Case 6 *** Sum of 2,3,4 should return 9 ***")
-        assert.equal(sumOfString("//[*][%]\n1*2%3"), 6, "Test Case 9 *** Sum of 1,2,3 should return 6 ***")
-        assert.equal(sumOfString("//[***]\n1***2***3"), 6, "Test Case 10 *** Sum of 1,2,3 should return 6 ***")
+        assert.equal(
+            sumOfString(""),
+            0,
+            "Test Case 1  *** Empty string should return 0 ***"
+        )
+        assert.equal(
+            sumOfString("2,3"),
+            5,
+            "Test Case 2  *** Sum of 2,3 should return 5 ***"
+        )
+        assert.equal(
+            sumOfString("2,3,4,5,6,7,8,9,10,99"),
+            (2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 99),
+            "Test Case 3 *** Sum of 2,3,4,5,6,7,8,9,10,99 should return 153 ***"
+        )
+        assert.throws(
+            () => sumOfString("-1,1,-3"),
+            Error,
+            "negative numbers not allowed -1,-3",
+            "Test Case 4 *** Negative numbers are not allowed ***"
+        )
+        assert.equal(
+            sumOfString("2,3\n4"),
+            9,
+            "Test Case 5 *** Sum of 2,3,4 should return 9 ***"
+        )
+        assert.equal(
+            sumOfString("//[*],2,3\n4"),
+            9,
+            "Test Case 6 *** Sum of 2,3,4 should return 9 ***"
+        )
+        assert.equal(
+            sumOfString("1000,2000,3,4"),
+            1007,
+            "Test Case 7 *** Sum of 1000,2000,3,4 should return 1007 ***"
+        )
+        assert.equal(
+            sumOfString("//[**],2**3\n4"),
+            9,
+            "Test Case 8 *** Sum of 2,3,4 should return 9 ***"
+        )
+        assert.equal(
+            sumOfString("//[#],2#3\n4"),
+            9,
+            "Test Case 6 *** Sum of 2,3,4 should return 9 ***"
+        )
+        assert.equal(
+            sumOfString("//[*][%]\n1*2%3"),
+            6,
+            "Test Case 9 *** Sum of 1,2,3 should return 6 ***"
+        )
+        assert.equal(
+            sumOfString("//[***]\n1***2***3"),
+            6,
+            "Test Case 10 *** Sum of 1,2,3 should return 6 ***"
+        )
 
-        assert.equal(sumOfString("//[***][%%]\n1***2%%3"), 6, "Test Case *** Sum of 1,2,3 should return 6 ***")
+        assert.equal(
+            sumOfString("//[***][%%]\n1***2%%3"),
+            6,
+            "Test Case *** Sum of 1,2,3 should return 6 ***"
+        )
         console.log("all test cases passed");
 
     } catch (err) {
